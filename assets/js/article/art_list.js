6a73f7b4fe7275f8a84afb50f788c982ec4413b4
skyblue <1104093356@qq.com>
2021-01-24 10:13:06 +0800

    完成发布文章页面

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,29 @@
+$(function () {
+  var layer = layui.layer;
+  // 定义一个查询的参数对象，将来请求数据的时候
+  // 需要将请求的参数对象提交到服务器
+  var q = {
+    pagenum: 1, // 页码值。默认请求第一页的数据
+    pagesize: 2, // 每页显示几条数据，默认每页显示俩条
+    cate_id: "", // 文字分类的id
+    state: "", // 文章的发布状态
+  };
+
+  initTable();
+  // 获取文字列表数据的方法
+  function initTable() {
+    $.ajax({
+      method: "GET",
+      url: "/my/article/list",
+      data: q,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("获取文字列表失败!");
+        }
+        // 使用模板引擎渲染页面的数据
+        var htmlStr = template("tpl-table", res);
+        $("tbody").html(htmlStr);
+      },
+    });
+  }
+});
